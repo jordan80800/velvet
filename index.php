95934eb69df01255ae16051b1641e08f08fd4972
jordan80800 <jordan80800@hotmail.fr>
2023-05-05 13:15:47 +0200

    il faut trouver erreur page index ajout recent

--- index.php
+++ index.php
@@ -18,38 +18,61 @@ $lesDisques = $requete->fetchAll(PDO::FETCH_ASSOC);
 
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><h1> Les Disques</h1></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      </ul>
-      <a href="ajout.php" class="btn btn-primary" type="button">Ajouter</a>
-    </div>
-  </div>
-</nav>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <h1> Les Disques</h1>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                </ul>
+                <a href="ajout.php" class="btn btn-primary" type="button">Ajouter</a>
+            </div>
+        </div>
+    </nav>
 
-    <div class="container-fluid g-0 h-100 d-flex align-items-center justify-content-center ">
+    <div class="container-fluid g-0 h-100  ">
+        <div id="recent">
+<?php
+$recent="SELECT *FROM discJOIN artist ON disc.artist_id=artist.artst_idORDER BY disc_id DESC LIMIT 5";
+$ajout=$db->query($recent);
+while($history_row=$ajout->fetch(PDO::FETCH_ASSOC)){ 
+    ?>
+        <div class="col-md-12">
+        <img src="assets/img/jaquettes/<?= $history_row['disc_picture'] ?>" class="img w-100 h-100 rounded-circle " alt="pourquoi sa s'affiche pas !!!!!!" id="image">
+        </div> 
+        <div>
+            <?php echo $history_row['disc_title'];?>
+        </div>
+        <div>
+            <?php echo $history_row['artist_name'];?>
+        </div>
+    
+}
 
 
-        <div class=" row  col-md-6 ">
+
+
+
+        </div>
+        <div class=" row  col-md-12 ">
             <?php foreach ($lesDisques as $undisque) : ?>
-                <div class="card col-md-6 border-0">
+                <div class="card col-md-6 border-end   " id="cardd">
                     <div class=" row g-0 ">
-                        <div class="col-md-6">
-                            <img src="assets/img/jaquettes/<?= $undisque['disc_picture'] ?>" class="img w-75 h-75 rounded-circle " alt="pourquoi sa s'affiche pas !!!!!!" id="image">
+                        <div class="col-md-4 d-flex justify-content-center align-items-center m-auto">
+                            <img src="assets/img/jaquettes/<?= $undisque['disc_picture'] ?>" class="img w-100 h-100 rounded-circle " alt="pourquoi sa s'affiche pas !!!!!!" id="image">
                         </div>
-                        <div class="col-md-6">
-                            <div class="card-body">
-                                <h5 class="card-title"><?= $undisque['disc_title'] ?></h5>
-                                <p class="card-text"><?= $undisque['artist_name']  ?></p>
-                                <p class="card-text"><?= $undisque['disc_label'] ?></p>
-                                <p class="card-text"><?= $undisque['disc_year'] ?></p>
-                                <p class="card-text"><?= $undisque['disc_genre'] ?></p>
-                                <input type="button" onclick="window.location.href='details.php?disc_id=<?=$undisque['disc_id']?>';" class="btn btn-danger btn" value="Détails"/>
+                        <div class="col-md-8 g-3 ">
+                            <div class="card-body ">
+                                <h5 class="card-title text-light "><?= $undisque['disc_title'] ?></h5>
+                                <p class="card-text text-light"><?= $undisque['artist_name']  ?></p>
+                                <p class="card-text text-light"><?= $undisque['disc_label'] ?></p>
+                                <p class="card-text text-light"><?= $undisque['disc_year'] ?></p>
+                                <p class="card-text text-light"><?= $undisque['disc_genre'] ?></p>
+                                <input type="button text-light" onclick="window.location.href='details.php?disc_id=<?= $undisque['disc_id'] ?>';" class="btn btn-danger btn" value="Détails" />
                             </div>
                         </div>
                     </div>
